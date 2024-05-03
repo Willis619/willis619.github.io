2deec10caed95978b1ecd0bfca6104dc61b1bce2
Willis619 <157516685+Willis619@users.noreply.github.com>
2024-05-03 16:04:52 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle);}
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        drawCircle();
 
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            // do something
+          }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +47,17 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+           
 
+            for (var i = 100; i < circles.length; i++) {
+                // code to repeat using i
+                physikz.updatePosition(circles[i])
+
+                game.checkCirclePosition(circles[i])
+              }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+          
 
             // TODO 9 : Iterate over the array
            
@@ -59,6 +74,15 @@ var init = function (window) {
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
+            }
+            if (circle.y > canvas.height) {
+                circle.y = 0
+            }
+            if (circle.x < 0) {
+                circle.x = 0
+            }
+            if (circle.y < 0) {
+                circle.y = 0
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
